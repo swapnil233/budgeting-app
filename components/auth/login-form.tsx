57a32c1d04dd8a9f11a2649a86a4ec242d258fb1
Hasan Iqbal <swapniliqbal@gmail.com>
2026-02-02 02:08:25 -0500

    refactor: update sign in page with shadcn components

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,93 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { signIn, useSession } from "@/lib/auth-client";
+import Link from "next/link";
+import { Spinner } from "../ui/spinner";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState<string | null>(null);
+
+  // auth check
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/dashboard");
+    }
+  });
+
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoadingError(null);
+    setLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+
+    const res = await signIn.email({
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    });
+
+    if (res.error) {
+      setLoadingError(res.error.message || "Something went wrong.");
+      setLoading(false);
+    } else {
+      router.push("/dashboard");
+    }
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input id="password" name="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit" disabled={loading}>
+                  {loading && <Spinner />}
+                  Login
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account?{" "}
+                  <Link href="/sign-up">Sign up</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
