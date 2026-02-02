6afe1da25c50b7023acc4b86436f1f237bc61239
Hasan Iqbal <swapniliqbal@gmail.com>
2026-02-02 00:40:23 -0500

    feat: add email and pass auth with better-auth. https://www.prisma.io/docs/guides/betterauth-nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  const router = useRouter();
+
+  return (
+    <main className="flex items-center justify-center h-screen bg-neutral-950 text-white">
+      <div className="flex gap-4">
+        <button
+          onClick={() => router.push("/sign-up")}
+          className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200"
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={() => router.push("/sign-in")}
+          className="border border-white text-white font-medium px-6 py-2 rounded-md hover:bg-neutral-800"
+        >
+          Sign In
+        </button>
+      </div>
+    </main>
+  );
 }
